5add02f36767973b7bf879c0759595020ff95ee4
etienne rabin <e.rabin05@gmail.com>
2019-04-11 16:30:10 +0200

    add mock to display list of projects

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -20,6 +20,40 @@ const projects = [
             'Edouard METZ'
         ]
     },
+    {
+        title : 'A project of my own making',
+        students : [
+            'Mathieu RABIN',
+            'samuel GILLES',
+            'justine DUBOIS',
+            'sebastien JOLLY'
+        ]
+    },
+    {
+        title : 'My other project Title',
+        students : [
+            'Julien RABIN',
+            'Maxime CORNIAU',
+            'Edouard METZ'
+        ]
+    },
+    {
+        title : 'A project of my own making',
+        students : [
+            'Mathieu RABIN',
+            'samuel GILLES',
+            'justine DUBOIS',
+            'sebastien JOLLY'
+        ]
+    },
+    {
+        title : 'My project Title',
+        students : [
+            'Etienne RABIN',
+            'Clement BECHETOILLE',
+            'Thomas CULDAUT'
+        ]
+    }
 ]
 
 const User = () => <ProjetcDisplay projects={projects}/>
