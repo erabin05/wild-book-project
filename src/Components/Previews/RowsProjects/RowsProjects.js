77858dca9414fdeb9b4d70b64e4c11e3ce2a7cec
etienne rabin <e.rabin05@gmail.com>
2019-04-13 22:29:03 +0200

    movment right and left and change opacity

--- src/Components/Previews/RowsProjects/RowsProjects.js
+++ src/Components/Previews/RowsProjects/RowsProjects.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './_rows-projects.scss'
 
 import  ProjectPreview from '../ProjectPreview/ProjectPreview'
@@ -15,19 +15,30 @@ const projectsInRowByFour = projects => {
             projectByfour = []
         } 
     })
-
     return projectsInRows
 }
 
-const RowsProjects = ({categorie, projects}) => (
+
+const RowsProjects = ({categorie, projects}) => {
+    const [isFocusedOn, setIsFocusedOn] = useState(0)
+    const [rowPosition, SetRowPosition] = useState(0)
+
+    return (
     <article className='rows-projects'>
         {projectsInRowByFour(projects).map((projectsByfour, i) => (
-                <div className='project-by-four' key={i} style={{marginLeft : `${10 + (i*80)}%`}}>
+                <div    className='project-by-four' 
+                        key={i} 
+                        style={{marginLeft : `${10 + (i*80) + rowPosition}%`, 
+                                opacity : isFocusedOn === i ? '1': '0.3'}}
+                >
                     {projectsByfour.map((project, j) => <ProjectPreview key={j} {...project}/>)}
                 </div>
             )
         )}
+        <button onClick={()=> {SetRowPosition(rowPosition - 80); setIsFocusedOn(isFocusedOn+1)}} >left</button>
+        <button onClick={()=> {SetRowPosition(rowPosition + 80); setIsFocusedOn(isFocusedOn-1)}}>rigth</button>
     </article> 
-)
+    )
+}
 
 export default RowsProjects
