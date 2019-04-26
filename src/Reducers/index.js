229783304668f707a89c3fbea96287af38689468
etienne rabin <e.rabin05@gmail.com>
2019-04-26 10:44:46 +0200

    delete unuse link

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,14 +1,12 @@
 import { combineReducers } from 'redux'
 
 import screenSize from './screenSize/reducer'
-import dedicatedCategorie from './dedicatedCategorie/reducer'
 import innerRowIdOfProjectSelected from './innerRowIdOfProjectSelected/reducer'
 import projectSelectedForFocus from './projectSelectedForFocus/reducer'
 import rowIdOfSelectedProject from './rowIdOfProjectSelected/reducer'
 
 export default combineReducers ({
     screenSize,
-    dedicatedCategorie,
     innerRowIdOfProjectSelected,
     projectSelectedForFocus,
     rowIdOfSelectedProject
