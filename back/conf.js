068469a3353e9153eba02050ca1e3b6bb1a9c8c6
etienne rabin <e.rabin05@gmail.com>
2019-05-07 14:57:17 +0200

    get all students, sessions, campuses

---
+++ back/conf.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql')
+require('dotenv').config()
+
+const connection = mysql.createConnection({
+    host : 'localhost',
+    user : process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: 'wild-portfolio'
+})
+
+module.exports = connection;
--- back/index.js
+++ back/index.js
@@ -1,11 +1,37 @@
-import express from 'express'
-import mysql from 'mysql'
-
+const express = require('express')
 const app = express()
 const port = 5000
 
-app.get('/students', (request, response) => {
+const connection = require('./conf');
 
+app.get('/students', (request, response) => {
+    connection.query('SELECT * FROM students',(err, results)=> {
+        if (err) {
+            response.status(500).send(`error when trying to get all students : ${err}`);
+          } else {
+            response.json(results);
+          }
+    })
+})
+
+app.get('/sessions', (request, response) => {
+    connection.query('SELECT * FROM sessions',(err, results)=> {
+        if (err) {
+            response.status(500).send(`error when trying to get all sessions : ${err}`);
+          } else {
+            response.json(results);
+          }
+    })
+})
+
+app.get('/campuses', (request, response) => {
+    connection.query('SELECT * FROM campuses',(err, results)=> {
+        if (err) {
+            response.status(500).send(`error when trying to get all campuses : ${err}`);
+          } else {
+            response.json(results);
+          }
+    })
 })
 
 app.listen(port, () => {
