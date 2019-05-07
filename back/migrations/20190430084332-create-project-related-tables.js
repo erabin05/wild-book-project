cfc9a0724c891168a8bab687a669b0834b1bac39
etienne rabin <e.rabin05@gmail.com>
2019-05-07 11:18:56 +0200

    create db

--- back/migrations/20190430084332-create-project-related-tables.js
+++ back/migrations/20190430084332-create-project-related-tables.js
@@ -15,6 +15,8 @@ exports.setup = function(options, seedLink) {
 };
 
 exports.up = function(db, callback) {
+
+  // CREATE TABLE PROJECTS
   db.createTable('projects', {
     id: 
       {
@@ -26,9 +28,10 @@ exports.up = function(db, callback) {
         length: 10
       },
     url: 'string',
-    name: 'string',
-    description: 'string',
+    name: {type: 'string', notNull: true},
+    description: 'text',
     githubLink: 'string',
+    imgLink: 'string',
     session_id:
       {
         type: 'int',
@@ -63,17 +66,115 @@ exports.up = function(db, callback) {
       }
   }, callback);
 
-  db.createTable('sessions', {
-    id: { type: 'int', primaryKey: true, autoIncrement: true },
-    name: 'string',
-    language_id:
+  // CREATE TABLE PROJECTS_HAS_STUDENTS
+  db.createTable('projects_has_students', {
+    projects_id:
       {
         type: 'int',
         unsigned: true,
         length: 10,
         notNull: true,
         foreignKey: {
-          name: 'sessions_language_id_fk',
+          name: 'projects_id_fk',
+          table: 'projects',
+          rules: {
+            onDelete: 'CASCADE',
+            onUpdate: 'RESTRICT'
+          },
+          mapping: 'id'
+        }
+      },
+    students_id:
+      {
+        type: 'int',
+        unsigned: true,
+        length: 10,
+        notNull: true,
+        foreignKey: {
+          name: 'students_id_fk',
+          table: 'students',
+          rules: {
+            onDelete: 'CASCADE',
+            onUpdate: 'RESTRICT'
+          },
+          mapping: 'id'
+        }
+      }
+  }, callback);
+
+  // CREATE TABLE SESSIONS
+  db.createTable('sessions', {
+    id: { type: 'int', primaryKey: true, autoIncrement: true },
+    name: {type: 'string', notNull: true},
+    date: 'datetime',
+    campuses_id:
+      {
+        type: 'int',
+        unsigned: true,
+        length: 10,
+        notNull: true,
+        foreignKey: {
+          name: 'sessions_campuses_id_fk',
+          table: 'campuses',
+          rules: {
+            onDelete: 'CASCADE',
+            onUpdate: 'RESTRICT'
+          },
+          mapping: 'id'
+        }
+      }
+  }, callback);
+
+  // CREATE TABLE STUDENTS
+  db.createTable('students', {
+    id: { type: 'int', primaryKey: true, autoIncrement: true },
+    name: 'string',
+    githubLink: 'string',
+    linkedinLink: 'string',
+    sessions_id:
+      {
+        type: 'int',
+        unsigned: true,
+        length: 10,
+        notNull: true,
+        foreignKey: {
+          name: 'students_sessions_id_fk',
+          table: 'sessions',
+          rules: {
+            onDelete: 'CASCADE',
+            onUpdate: 'RESTRICT'
+          },
+          mapping: 'id'
+        }
+      }
+  }, callback);
+
+  // CREATE TABLE SESSIONS_HAS_LANGUAGES
+  db.createTable('sessions_has_languages', {
+    sessions_id:
+      {
+        type: 'int',
+        unsigned: true,
+        length: 10,
+        notNull: true,
+        foreignKey: {
+          name: 'sessions_id_fk',
+          table: 'sessions',
+          rules: {
+            onDelete: 'CASCADE',
+            onUpdate: 'RESTRICT'
+          },
+          mapping: 'id'
+        }
+      },
+    languages_id:
+      {
+        type: 'int',
+        unsigned: true,
+        length: 10,
+        notNull: true,
+        foreignKey: {
+          name: 'language_id_fk',
           table: 'languages',
           rules: {
             onDelete: 'CASCADE',
@@ -84,24 +185,30 @@ exports.up = function(db, callback) {
       }
   }, callback);
 
+  // CREATE TABLE LANGUAGES
   db.createTable('languages', {
     id: { type: 'int', primaryKey: true, autoIncrement: true },
     name: 'string'
   }, callback);
 
-  db.createTable('students', {
+  // CREATE TABLE CAMPUSES
+  db.createTable('campuses', {
     id: { type: 'int', primaryKey: true, autoIncrement: true },
-    name: 'string',
-    githubLink: 'string',
-    linkedinLink: 'string'
+    name: {type: 'string', notNull: true},
+    coordonates: 'string'
   }, callback);
+
 };
 
 exports.down = function(db, callback) {
   db.removeTable('projects', callback)
-  db.removeTable('sessions', callback)
-  db.removeTable('languages', callback)
+  db.removeTable('projects_has_students', callback)
   db.removeTable('students', callback)
+  db.removeTable('sessions', callback)
+  db.removeTable('sessions_has_languages', callback)
+  db.removeTable('languages', callback)
+  db.removeTable('campuses', callback)
+  
 };
 
 exports._meta = {
