53ebdef3547a12ce6972238bca447e8a5431f612
etienne rabin <e.rabin05@gmail.com>
2019-05-10 12:12:41 +0200

    github link and linkedin link on student

--- src/Components/Previews/StudentPreview/StudentPreview.js
+++ src/Components/Previews/StudentPreview/StudentPreview.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './_student-preview.scss'
 import wildProfilPic from './wildProfilPic'
 
-const StudentPreview = ({name}) => (
-    <a className='student-preview'>
-        <span>{wildProfilPic()}</span>
-        {name}
-    </a>
-)
+const StudentPreview = ({name, github, linkedin}) => {
+    const [isMouseOver, setIsMouseOver] = useState(false)    
+    return (
+        <div className='student-preview' onMouseEnter={()=>setIsMouseOver(true)} onMouseLeave={()=>setIsMouseOver(false)}>
+        
+            <span>{wildProfilPic()}</span>
+            {isMouseOver
+                ?<div className='links'>
+                    <a href={github} rel='noreferrer noopener' target='_blank'>Github</a>
+                    <a href={linkedin} rel='noreferrer noopener' target='_blank'>Linkedin</a>
+                </div>
+                :<p>{name}</p>
+            }
+        </div>
+    )
+}
 
 export default StudentPreview
