446aade38ef3c6140503f1bbcdbe7899a7d7315a
etienne rabin <e.rabin05@gmail.com>
2019-05-29 14:31:32 +0200

    dynamic unrol list period

--- src/Components/Admin/Session/Period/Period.js
+++ src/Components/Admin/Session/Period/Period.js
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, {useState, useRef} from 'react'
 import './_period.scss'
 
-const Period = ({name}) => (
-    <section className='admin-period'>
-        <div>{name}</div>
-    </section>
-)
+import ProjectLine from './ProjectLine'
+
+const Period = ({name}) => {
+    const [isSelected, setIsSelected] = useState(false)
+    const inputEl = useRef(null);
+
+    const projects = [
+        {
+            title : 'The title of my great project',
+            imgLink : '',
+            url : 'http://localhost:3000',
+            github : 'https://github.com/',
+            students : [
+                {
+                    name : 'Clément Bechetoille'
+                },
+                {
+                    name : 'Romain Guillemot'
+                },
+                {
+                    name : 'Etienne Rabin'
+                },
+            ]
+        },
+    ]
+
+    return (
+        <section className='admin-period'>
+            <div onClick={() => setIsSelected(isSelected => !isSelected)}>
+                <p className={isSelected ? 'select': 'unselect'}>{name}</p>
+            </div>
+            <section className='projects-list-container' style={{height : isSelected && `${inputEl.current.offsetHeight}px`}}>
+                <div className='projects-list'ref={inputEl} >
+                    <div>
+                        <button className='outline-button'>Add project</button>
+                    </div>
+                    {projects.map((project, index) => <ProjectLine key={index} {...project}/>)}
+                </div>
+            </section>
+        </section>
+    )
+}
+
 
 export default Period
---
+++ src/Components/Admin/Session/Period/ProjectLine.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProjectLine = ({title}) => (
+    <section className='admin-project-line'>
+        <figure></figure>
+        <div>
+            <p>{title}</p>
+        </div>
+        <div>
+            
+        </div>
+    </section>
+)
+
+export default ProjectLine
